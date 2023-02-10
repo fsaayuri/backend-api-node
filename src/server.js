67a4b697ac5fff84e7a18a5855fccfb676cf8bc3
Fernanda Sayuri <fernandasayuri.160@gmail.com>
2023-02-10 15:30:51 -0300

    teste de como foi feito request e response

--- src/server.js
+++ src/server.js
@@ -4,16 +4,33 @@
 // server.listen criou uma função no servidor para escutar na porta 3100 nesse host (e coloca uma string avisando para o usuário)
 
 const http = require('node:http');
+// importar pacotes nativos e instalados (require)
+const fs = require('fs')
+
 
 const hostname = '127.0.0.1';
 const port = 3100;
 
+// o node da uma função dentro do http de criar um servidor, após um ponto vem uma das funções da biblioteca | SÓ FOI PREPARADO O RESPONSE (RESPOSTA)
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
+  // request (requisição) que foi o clique e ele responde com um response (resposta) o que foi solicitado
+  console.log('URL:' + req.url)
+  if(req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('src/views/index.html', (error, codigo) => {
+          res.end(codigo);
+    })
+
+  } else if(req.url === '/cursos'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('Bem vindo aos cursos!');
+  }
+  // favicon ele já faz um request pra tentar pegar o icone do navegador
 });
 
+// como se fosse um apache rodando dentro do xampp ele fica "Escutando" alterações para o backend dar uma resposta
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
